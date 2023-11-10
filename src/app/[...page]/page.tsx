61caf0bce034b374bfb664329ed5c5751754fefe
Rainstorm Studio Australia <paul.tran@rainstormstudio.com.au>
2023-11-10 22:31:45 +0700

    init

---
+++ src/app/[...page]/page.tsx
@@ -0,0 +1,35 @@
+// Example file structure, app/[...page]/page.tsx
+// You could alternatively use src/app/[...page]/page.tsx
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "@/components/builder";
+
+// Replace with your Public API Key
+builder.init("1c899a6e15234ef8bafcd5c88d4f3cbe");
+
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+export default async function Page(props: PageProps) {
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get("page", {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+      },
+      // Set prerender to false to return JSON instead of HTML
+      prerender: false,
+    })
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} />
+    </>
+  );
+}
